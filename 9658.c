d6058207cc79522d437e7c8d73a98b8cbca8dafa
Moon Sang Jin <moonsoo5522@naver.com>
2017-02-11 19:21:22 +0900

    9658

---
+++ 9658.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, dp[1001]={0,};
+    scanf("%d", &num);
+    dp[1]=dp[3]=1;
+    
+    for(int i=4; i<=num; i++)
+        if(!dp[i-1] && !dp[i-3] && !dp[i-4])
+            dp[i]=1;
+    printf(dp[num]?"CY":"SK");
+    return 0;
+}
